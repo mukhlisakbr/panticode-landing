5fc99ff10d408d1a89966c2c69c0842913102935
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2022-04-08 05:18:06 +0700

    feat: be responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,59 +15,92 @@ const Home: NextPage = () => {
         description="We make cool & fast apps. Committed to helping people with useful free apps."
       />
       <div className="min-h-screen bg-black">
-        <div className="mx-auto max-w-7xl px-4 py-12">
+        <div className="mx-auto max-w-7xl px-8 py-12">
           <nav className="flex items-center justify-between">
-            <div className="">
+            <div className="relative h-12 w-6/12 md:h-20 md:w-64">
               <a href="#">
-                <Image src={Logo} alt="Logo"></Image>
+                <Image
+                  src={Logo}
+                  alt="Logo"
+                  objectFit="contain"
+                  layout="fill"
+                ></Image>
               </a>
             </div>
-            <div>
+            <div className="">
               <a
                 href="https://play.google.com/store/apps/dev?id=6540392924092933918"
                 target={'_blank'}
                 rel="noreferrer"
               >
-                <button className="flex items-center gap-2 rounded-full bg-gradient-to-r from-blue-400 to-emerald-400 py-3 px-8 text-2xl font-bold tracking-tight text-gray-50">
-                  <Image src={GooglePlayIcon} alt="Icon"></Image>find us.
+                <button className="flex items-center gap-2 rounded-full bg-gradient-to-r from-blue-400 to-emerald-400 px-4 py-2 text-base font-bold tracking-tight text-gray-50 md:py-3 md:px-8 md:text-2xl">
+                  <span className="relative h-4 w-4 md:h-8 md:w-8">
+                    <Image
+                      src={GooglePlayIcon}
+                      alt="Icon"
+                      objectFit="contain"
+                      layout="fill"
+                    ></Image>
+                  </span>
+                  <span>find us.</span>
                 </button>
               </a>
             </div>
           </nav>
-          <main className="mt-28 flex items-center">
-            <div className="w-6/12 flex-col text-gray-50">
-              <div className="font-heading text-7xl font-bold">
+          <main className="mt-20 flex items-center md:mt-28">
+            <div className="flex-col text-gray-50 sm:w-full md:w-6/12">
+              <div className="w-44 justify-end md:hidden">
+                <Image
+                  src={Hero}
+                  alt="Hero"
+                  width={500}
+                  objectFit="contain"
+                ></Image>
+              </div>
+              <div className="font-heading text-4xl font-bold md:text-7xl">
                 <h1>We make</h1>
                 <h1 className="bg-gradient-to-r from-blue-400 to-emerald-400 bg-clip-text text-transparent">
                   cool & fast apps.
                 </h1>
               </div>
-              <div className="mt-8 text-xl ">
+              <div className="mt-4 text-sm md:mt-8 md:text-xl ">
                 Committed to helping people with useful free apps.
               </div>
               <div>
-                <div className="mt-20 font-bold tracking-tight text-gray-600">
+                <div className="mt-10 text-xs font-bold tracking-tight text-gray-400 md:mt-20 md:text-base">
                   POWERED BY THE LATEST TECHNOLOGY IN THE INDUSTRY
                 </div>
-                <div className="mt-4 flex gap-2">
+                <div className=" mt-4 flex gap-2 ">
                   <a
                     href="https://reactnative.dev/"
                     target={'_blank'}
                     rel="noreferrer"
+                    className="relative h-10 w-10  md:h-16 md:w-16"
                   >
-                    <Image src={ReactNativeLogo} alt="React Native"></Image>
+                    <Image
+                      src={ReactNativeLogo}
+                      alt="React Native"
+                      layout="fill"
+                      objectFit="contain"
+                    ></Image>
                   </a>
                   <a
                     href="https://expo.dev/"
                     target={'_blank'}
                     rel="noreferrer"
+                    className="relative h-10 w-10  md:h-16 md:w-16"
                   >
-                    <Image src={ExpoLogo} alt="Expo"></Image>
+                    <Image
+                      src={ExpoLogo}
+                      alt="Expo"
+                      layout="fill"
+                      objectFit="contain"
+                    ></Image>
                   </a>
                 </div>
               </div>
             </div>
-            <div className="flex w-6/12 justify-end ">
+            <div className="hidden w-6/12 justify-end md:block">
               <Image
                 src={Hero}
                 alt="Hero"
@@ -76,7 +109,7 @@ const Home: NextPage = () => {
               ></Image>
             </div>
           </main>
-          <footer className="mt-12 flex justify-end gap-2  tracking-widest text-gray-600">
+          <footer className="mt-12 flex justify-end gap-2 text-xs tracking-widest  text-gray-400 md:text-base">
             <a
               href="https://id.wikipedia.org/wiki/Daerah_Istimewa_Yogyakarta"
               target={'_blank'}
